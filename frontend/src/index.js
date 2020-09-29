d32344f26f42f3d1893b4fcb151a5503f4d22c49
ariannmichael <ariannmichael@gmail.com>
2020-09-29 16:08:44 -0300

    Frontend configuration

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,11 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Router from './Router';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Router/>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
