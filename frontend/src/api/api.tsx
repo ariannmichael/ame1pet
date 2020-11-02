38a917bed5d94f343b93355496df37dc31acb042
ariannmichael <ariannmichael@gmail.com>
2020-11-02 03:06:27 -0300

    Services to communicate with backend

---
+++ frontend/src/api/api.tsx
@@ -0,0 +1,22 @@
+import axios from "axios";
+import PetModel from "../models/PetModel";
+
+const baseURL = "http://localhost:8080/"
+
+export const getAllPets = async() => {
+    const url = baseURL + "pets";
+
+    return axios.get(url);
+}
+
+export const getPetById = async(id: number) => {
+    const url = baseURL + "pets/" + id;
+
+    return axios.get(url);
+}
+
+export const postPet = async(body: PetModel) => {
+    const url = baseURL + "pets";
+
+    return axios.post(url, body);
+}
