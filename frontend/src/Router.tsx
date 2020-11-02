9ec3d16f0d5771ab4e2b2b5406b2c85252cc6354
ariannmichael <ariannmichael@gmail.com>
2020-11-02 20:51:57 -0300

    Update router

--- frontend/src/Router.tsx
+++ frontend/src/Router.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { Switch, Route, Redirect } from 'react-router-dom';
 import AddPet from "./components/pages/AddPet";
-import DashboardComponent from "./components/pages/Dashboard";
+import Dashboard from "./components/pages/Dashboard/Dashboard";
 import Pet from "./components/pages/Pet";
 
 const Router = () => {
     return (
         <Switch>
-            <Route exatc path="/dashboard" component={DashboardComponent}/>
+            <Route exatc path="/dashboard" component={Dashboard}/>
             <Route path="/pet" component={Pet}/>
             <Route path="/add" component={AddPet}/>
             <Redirect from="*" to="/dashboard"/>
